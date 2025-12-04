7293aa05d9f73177ed0b08c6d7e908c6db332d40
minjunkim0205 <minjunkim0205@gmail.com>
2025-12-04 22:33:10 +0900

    Init project

---
+++ Source/GravityShift/Private/GM_Game.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GM_Game.h"
+#include "GravityShift.h"
+
+AGM_Game::AGM_Game()
+{
+	//UE_LOG(LogTemp, Warning, TEXT("%s"), *CALLINFO);
+	//PRINT_CALLINFO();
+	PRINT_LOG(TEXT("My Log : %s"), TEXT("GravityShift Log test"));
+}
---
+++ Source/GravityShift/Private/GravityShift.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GravityShift.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GravityShift, "GravityShift" );
+
+DEFINE_LOG_CATEGORY(GravityShiftLog);
